ca3482675e702130970a58c388f75a9444842cf3
Matt Harris <mateodelay@gmail.com>
2015-10-12 16:45:26 -0600

    Changed info about matt

--- Index.php
+++ Index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE.html>
 <title> This page is about Matt</title>
 <body>
-	<h1>Matt was able to get this minimal excuse for a webpage online</h1>
+	<h1>Matt was able to get this sentence published to the internets</h1>
 
 </body>
 
