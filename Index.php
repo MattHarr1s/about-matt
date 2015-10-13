7073e574658cfd7fca525f4c2cf45569e6308faa
Matt Harris <mateodelay@gmail.com>
2015-10-13 11:02:35 -0600

    Tried to fix link for Matt and Sawyer image.

--- Index.php
+++ Index.php
@@ -4,8 +4,9 @@
 		<meta charset="utf-8"/>
 			<title>About Matt</title>
 				</head>
-					<main>
-						<h1>Matt Harris</h1>
-						<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp. Matt decided to learn web development on a whim, after spending time in several different fields Matt has worked as a wrestling coach, wrestling camp counselor,valet attendant, emergency medical technician, electrician's helper, property preservation specialist, and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development, he enjoys spending time with his wife Anita and their 14 month old son Sawyer. </p>
-					</main>
+						<main>
+							<h1>Matt Harris</h1>
+							<img src="/img/mattsawyer.jpg" about="Matt and Sawyer/>"
+								<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp. Matt decided to learn web development on a whim, after spending time in several different fields Matt has worked as a wrestling coach, wrestling camp counselor,valet attendant, emergency medical technician, electrician's helper, property preservation specialist, and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development, he enjoys spending time with his wife Anita and their 14 month old son Sawyer. </p>
+						</main>
 </html>
