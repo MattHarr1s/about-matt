2d84e16d09e7457a9f3900d666cbafdfdcbd6fab
Matt Harris <mateodelay@gmail.com>
2015-10-13 13:22:32 -0600

    Added body tag and changed about to alt.

--- Index.php
+++ Index.php
@@ -4,15 +4,16 @@
 		<meta charset="utf-8"/>
 			<title>About Matt</title>
 				</head>
-					<main>
-						<h1>Matt Harris</h1>
-							<a href="img/mattsawyer.jpg" target="_blank"><img src="img/mattsawyer.jpg"  about="Matt and Sawyer" width="25%" height="25%"/></a>
-								<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp.
-									Matt decided to learn web development on a whim, after spending time in
-									several different fields. Matt has worked as a wrestling coach, wrestling camp counselor,
-									valet attendant, emergency medical technician, electrician's helper, property preservation specialist,
-									and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development,
-									he enjoys spending time with his wife Anita and their 14 month old son Sawyer or enjoying one of his several hobbies listed below. </p>
+					<body>
+						<main>
+							<h1>Matt Harris</h1>
+								<a href="img/mattsawyer.jpg" target="_blank"><img src="img/mattsawyer.jpg"  alt="Matt and Sawyer" width="25%" height="25%"/></a>
+									<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp.
+										Matt decided to learn web development on a whim, after spending time in
+										several different fields. Matt has worked as a wrestling coach, wrestling camp counselor,
+										valet attendant, emergency medical technician, electrician's helper, property preservation specialist,
+										and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development,
+										he enjoys spending time with his wife Anita and their 14 month old son Sawyer or enjoying one of his several hobbies listed below. </p>
 										<h2>Work Experience</h2>
 											<p>Matt's past jobs are extremely varied. Below is a list of highlights from his work history.</p>
 												<ul>
@@ -24,7 +25,7 @@
 													<li>Student at Deep Dive Coding Bootcamp Fall 2015</li>
 												</ul>
 													<h3>Hobbies</h3>
-														 <a href="img/mattsawyerdavidskiing.jpg" target="_blank"><img src="img/mattsawyerdavidskiing.jpg" about="Matt Sawyer and David at DMR" width="50%" height="50%"/></a>
+														 <a href="img/mattsawyerdavidskiing.jpg" target="_blank"><img src="img/mattsawyerdavidskiing.jpg" alt="Matt Sawyer and David at DMR" width="50%" height="50%"/></a>
 															<p>Matt has several hobbies which are listed below in no particular order.</p>
 																<ol>
 																	<li><a href="http://www.skinet.com/warrenmiller/" target="_blank">Skiing</a></li>
@@ -36,4 +37,5 @@
 																	<li>Coding!</li>
 																</ol>
 						</main>
+					</body>
 </html>
