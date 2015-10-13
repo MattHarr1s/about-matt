265502cf6a9e3f5213a531d38a0becfa8d68a22e
Matt Harris <mateodelay@gmail.com>
2015-10-13 10:23:55 -0600

    Added two links using the anchor tag, one is supposed to open in the same window and the second is supposed to open in a new window.

--- Index.php
+++ Index.php
@@ -6,5 +6,7 @@
 	</head>
 	<body>
 		<h1>Matt successfully <em>published</em> this sentence to the internets!</h1>
+		<a href="https://google.com">Google</a>
+		<a href="https://google.com" target="_blank">Google</a>
 	</body>
 </html>
