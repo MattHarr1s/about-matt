dd3b13356983d679e435c8653768a602af4839f6
Matt Harris <mateodelay@gmail.com>
2015-10-13 12:53:47 -0600

    Made first image into a clickable link and worked with formatting.

--- Index.php
+++ Index.php
@@ -4,15 +4,15 @@
 		<meta charset="utf-8"/>
 			<title>About Matt</title>
 				</head>
-						<main>
-							<h1>Matt Harris</h1>
-							<img src="img/mattsawyer.jpg" about="Matt and Sawyer" width="50%" height="50%"/>
+					<main>
+						<h1>Matt Harris</h1>
+							<a href="img/mattsawyer.jpg" target="_blank"><img src="img/mattsawyer.jpg"  about="Matt and Sawyer" width="50%" height="50%"/></a>
 								<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp.
 									Matt decided to learn web development on a whim, after spending time in
 									several different fields. Matt has worked as a wrestling coach, wrestling camp counselor,
 									valet attendant, emergency medical technician, electrician's helper, property preservation specialist,
 									and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development,
-									he enjoys spending time with his wife Anita and their 14 month old son Sawyer. </p>
+									he enjoys spending time with his wife Anita and their 14 month old son Sawyer or enjoying one of his several hobbies listed below. </p>
 										<h2>Work Experience</h2>
 											<p>Matt's past jobs are extremely varied. Below is a list of highlights from his work history.</p>
 												<ul>
@@ -24,7 +24,7 @@
 													<li>Student at Deep Dive Coding Bootcamp Fall 2015</li>
 												</ul>
 													<h3>Hobbies</h3>
-														 <a href="img/mattsawyerdavidskiing.jpg" target="_blank"><img src="img/mattsawyerdavidskiing.jpg" about="Matt Sawyer and David at DMR" width="50%"height="50%"/></a>
+														 <a href="img/mattsawyerdavidskiing.jpg" target="_blank"><img src="img/mattsawyerdavidskiing.jpg" about="Matt Sawyer and David at DMR" width="50%" height="50%"/></a>
 															<p>Matt has several hobbies which are listed below in no particular order</p>
 																<ol>
 																	<li>Skiing</li>
@@ -32,7 +32,7 @@
 																	<li>Spending Time With Family</li>
 																	<li>Practicing Martial Arts</li>
 																	<li>Performing Stand-Up Comedy</li>
-																	<li>Tinkering with Cars and Electronics</li>
+																	<li>Repairing and Modding Cars and Electronics</li>
 																	<li>Coding!</li>
 																</ol>
 						</main>
