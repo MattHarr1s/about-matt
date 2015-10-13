f44b86da4585ce25d2c17a3b5c6b11e1b44868e5
Matt Harris <mateodelay@gmail.com>
2015-10-13 10:49:35 -0600

    Increased amount of information in About Matt paragraph.

--- Index.php
+++ Index.php
@@ -2,10 +2,10 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>About Matt</title>
-	</head>
-	<body>
-		<h1>Matt Harris</h1>
-			<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp. When Matt isn't studying web development,he enjoys spendng time with his wife Anita and their 14 month old son Sawyer. </p>
-	</body>
+			<title>About Matt</title>
+				</head>
+					<main>
+						<h1>Matt Harris</h1>
+						<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp. Matt decided to learn web development on a whim, after spending time in several different fields Matt has worked as a wrestling coach, wrestling camp counselor,valet attendant, emergency medical technician, electrician's helper, property preservation specialist, and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development, he enjoys spending time with his wife Anita and their 14 month old son Sawyer. </p>
+					</main>
 </html>
