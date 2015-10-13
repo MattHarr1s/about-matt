7476063a9001e2ae868fe10057c008f64626a56c
Matt Harris <mateodelay@gmail.com>
2015-10-13 13:43:22 -0600

    Added footer and twitter link.

--- Index.php
+++ Index.php
@@ -38,5 +38,5 @@
 																</ol>
 						</main>
 					</body>
-	<footer><a href="https://twitter.com/Matt636" target="_blank">@matt636</a> </footer>
+	<footer><h4>Follow Me on Twitter at:<a href="https://twitter.com/Matt636" target="_blank">@matt636</a></h4> </footer>
 </html>
