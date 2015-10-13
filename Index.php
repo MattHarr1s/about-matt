38bdc32b463af93ff635564ea3e33cbd3786f14e
Matt Harris <mateodelay@gmail.com>
2015-10-13 10:39:48 -0600

    Added paragraph tag and some info about Matt

--- Index.php
+++ Index.php
@@ -5,8 +5,7 @@
 		<title>About Matt</title>
 	</head>
 	<body>
-		<h1>Matt successfully <em>published</em> this sentence to the internets!</h1>
-		<a href="https://google.com">Google</a>
-		<a href="https://google.com" target="_blank">Google</a>
+		<h1>Matt Harris</h1>
+			<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp. When Matt isn't studying web development,he enjoys spendng time with his wife Anita and their 14 month old son Sawyer. </p>
 	</body>
 </html>
