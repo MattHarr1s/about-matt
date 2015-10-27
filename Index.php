df3a7b72e006e162e2f9ca9bdb12a1f8c46dd5e0
Matt Harris <mateodelay@gmail.com>
2015-10-27 13:12:50 -0600

    Created stylesheet and added h1 and p styles.

--- Index.php
+++ Index.php
@@ -2,31 +2,34 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-			<title>About Matt</title>
-				</head>
-					<body>
-						<main>
-							<h1>Matt Harris</h1>
-								<a href="img/mattsawyer.jpg" target="_blank"><img src="img/mattsawyer.jpg"  alt="Matt and Sawyer" width="25%" height="25%"/></a>
-									<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp.
-										Matt decided to learn web development on a whim, after spending time in
-										several different fields. Matt has worked as a wrestling coach, wrestling camp counselor,
-										valet attendant, emergency medical technician, electrician's helper, property preservation specialist,
-										and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development,
-										he enjoys spending time with his wife Anita and their 14 month old son Sawyer or enjoying one of his several hobbies listed below. </p>
-										<h2>Work Experience</h2>
-											<p>Matt's past jobs are extremely varied. Below is a list of highlights from his work history.</p>
-												<ul>
-													<li>Wrestling Coach from 2000 to 2001</li>
-													<li>Valet Attendant from 2002 to 2007</li>
-													<li>Emergency Medical Technician/Paramedic from 2007 to 2010</li>
-													<li>Property Preservation Specialist from 2010 to 2012</li>
-													<li>Paralegal, Office Manager and IT from 2012 to 2015</li>
-													<li>Student at Deep Dive Coding Bootcamp Fall 2015</li>
-												</ul>
-													<h3>Hobbies</h3>
-														 <a href="img/mattsawyerdavidskiing.jpg" target="_blank"><img src="img/mattsawyerdavidskiing.jpg" alt="Matt Sawyer and David at DMR" width="50%" height="50%"/></a>
-															<p>Matt has several hobbies which are listed below in no particular order.</p>
+		<title>About Matt</title>
+	</head>
+	<body>
+		<h1>Matt Harris</h1>
+				<a href="img/mattsawyer.jpg" target="_blank"><img src="img/mattsawyer.jpg"  alt="Matt and Sawyer" width="25%"
+				height="25%"/>
+				</a>
+			<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp.
+			Matt decided to learn web development on a whim, after spending time in
+			several different fields. Matt has worked as a wrestling coach, wrestling camp counselor,
+			valet attendant, emergency medical technician, electrician's helper, property preservation specialist,
+			and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development,
+			he enjoys spending time with his wife Anita and their 14 month old son Sawyer or enjoying one of his several
+			hobbies listed below.
+			</p>
+			<h2>Work Experience</h2>
+				<p>Matt's past jobs are extremely varied. Below is a list of highlights from his work history.</p>
+					<ul>
+						<li>Wrestling Coach from 2000 to 2001</li>
+						<li>Valet Attendant from 2002 to 2007</li>
+						<li>Emergency Medical Technician/Paramedic from 2007 to 2010</li>
+						<li>Property Preservation Specialist from 2010 to 2012</li>
+						<li>Paralegal, Office Manager and IT from 2012 to 2015</li>
+						<li>Student at Deep Dive Coding Bootcamp Fall 2015</li>
+					</ul>
+			<h3>Hobbies</h3>
+				<a href="img/mattsawyerdavidskiing.jpg" target="_blank"><img src="img/mattsawyerdavidskiing.jpg" alt="Matt Sawyer and David at DMR" width="50%" height="50%"/></a>
+				<p>Matt has several hobbies which are listed below in no particular order.</p>
 																<ol>
 																	<li><a href="http://www.skinet.com/warrenmiller/" target="_blank">Skiing</a></li>
 																	<li><a href="http://losangeles.dodgers.mlb.com/index.jsp?c_id=la" target="_blank"> Watching Baseball</a></li>
