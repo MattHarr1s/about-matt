c1d6e73b6d51010ea70db93d67d762993264fa0a
Matt Harris <mateodelay@gmail.com>
2015-10-29 13:44:09 -0600

    Added bootstrap

---
+++ index-bootstrap.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- ////////////////////////////////////////////////
+		//// LINK TO YOUR CUSTOM CSS FILES HERE
+		//// <link type="text/css" href="css/style.css" rel="stylesheet"/>
+		///////////////////////////////////////////////////// -->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- js-cookie -->
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<title>About Matt</title>
+	</head>
+	<body>
+		<div class="container">
+			<h1>Matt Harris</h1>
+			<div class="image">
+				<a href="img/mattsawyer.jpg" target="_blank"><img src="img/mattsawyer.jpg"  alt="Matt and Sawyer" />
+			</div>
+			<div class="firstParagraph">
+				<p>Matt Harris is a 33 year old student in the Deep Dive Coding Bootcamp.
+					Matt decided to learn web development on a whim, after spending time in
+					several different fields. Matt has worked as a wrestling coach, wrestling camp counselor,
+					valet attendant, emergency medical technician, electrician's helper, property preservation specialist,
+					and most recently as a paralegal and office manager of a small law firm. When Matt isn't studying web development,
+					he enjoys spending time with his wife Anita and their 14 month old son Sawyer or enjoying one of his several
+					hobbies listed below.
+				</p>
+			</div>
+			<div class="workExperience">
+				<h2>Work Experience</h2>
+				<p>Matt's past jobs are extremely varied. Below is a list of highlights from his work history.</p>
+				<ul>
+					<li>Wrestling Coach from 2000 to 2001</li>
+					<li>Valet Attendant from 2002 to 2007</li>
+					<li>Emergency Medical Technician/Paramedic from 2007 to 2010</li>
+					<li>Property Preservation Specialist from 2010 to 2012</li>
+					<li>Paralegal, Office Manager and IT from 2012 to 2015</li>
+					<li>Student at Deep Dive Coding Bootcamp Fall 2015</li>
+				</ul>
+			</div>
+			<div class=""
+		</div>
+	</body>
+</html>
